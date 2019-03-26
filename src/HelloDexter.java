e9680fc37cba9bb0a34e365f4854ec8b002b820d
saravanan7 <saravanan7be@gmail.com>
2019-03-25 22:27:31 -0500

    Update HelloDexter.java

--- src/HelloDexter.java
+++ src/HelloDexter.java
@@ -3,7 +3,7 @@ public class HelloDexter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Dexter !! GoodLuck on Jenkins Learning !!");
+		System.out.println("Hello Dexter !! GoodLuck on Jenkins Learning !! Added code to trigger automatically using POLL SCM");
 	}
 
 }
